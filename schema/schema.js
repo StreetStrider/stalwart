41db2296df1ee3f859833971076a34205367f382
StreetStrider <teamfortresslife@gmail.com>
2015-10-16 22:32:46 +0300

    split schema json.

--- schema/schema.js
+++ schema/schema.js
@@ -3,22 +3,17 @@
 
 var C = require('chroma-js')
 
-module.exports =
-{
-	fg: C('333'),
-	bg: C('fbfbfb'),
+var schema = module.exports = {}
 
-	accent:
-	{
-		fg: C('389940'),
-		bg: C('18BD23')
-	},
-
-	accent__b:
-	{
-		fg: C('2C7376'),
-		bg: C('148B91')
-	}
-}
+schema.fg = C('333')
+schema.bg = C('fbfbfb')
 
 // http://paletton.com/#uid=52Q0u0kkiopKBJ1rSu5d+k46Kft
+
+schema.accent = {}
+schema.accent.fg = fg = C('389940')
+schema.accent.bg = bg = C('18BD23')
+
+schema.accent__b = {}
+schema.accent__b.fg = fg = C('2C7376')
+schema.accent__b.bg = bg = C('148B91')
