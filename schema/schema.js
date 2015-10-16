4d6ea4b30905e4b8fa61cc2e7ed395e8e978429c
StreetStrider <teamfortresslife@gmail.com>
2015-10-16 22:30:59 +0300

    accents.

--- schema/schema.js
+++ schema/schema.js
@@ -5,6 +5,20 @@ var C = require('chroma-js')
 
 module.exports =
 {
+	fg: C('333'),
 	bg: C('fbfbfb'),
-	fg: C('333')
+
+	accent:
+	{
+		fg: C('389940'),
+		bg: C('18BD23')
+	},
+
+	accent__b:
+	{
+		fg: C('2C7376'),
+		bg: C('148B91')
+	}
 }
+
+// http://paletton.com/#uid=52Q0u0kkiopKBJ1rSu5d+k46Kft
